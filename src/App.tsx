75f2776c29adef364be70620e8d4625639f856ee
Thaylon Haniel <thaylonhaniel9@gmail.com>
2022-08-13 22:46:22 -0300

    inputCreation

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,22 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import './App.css'
+
+import "./assets/styles/global.css"
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div >
+      <header  className="bg-gray-800 text-red-900 p-8">
+      <h1 className="my-5 text-center text-3xl bg-transparent" >
+       Videos<strong id="strongHeader" className="text-zinc-900 bg-transparent">Listing</strong>
+      </h1>
+      </header>
+      <main>
+            <h2>pesquise o canal em que quer listar o vídeo:</h2>
+            <input type="text"/>
+      </main>
     </div>
   )
 }
